89b16e3f72772b1d0801972d42bb2e69f51c6907
Abdul Basit <abdulbasitswl2002@gmail.com>
2026-05-04 23:45:22 +0500

    first commit

---
+++ SumOfArray.js
@@ -0,0 +1,18 @@
+
+// FOR LOOP 
+// let number=[1,2,4,5];
+// let sum=0;
+// for(let i=0;i<number.length;i++)
+// {
+//     sum=sum+number[i];
+// }
+// console.log("Sum of array is "+sum);
+
+
+// FOR EACH
+// let number=[2,3,4,5,6];
+// let sum=0;
+// number.forEach(value=>sum=sum+value);
+// console.log("Sum of array is "+sum);
+
+// Reduce function
