ef118bf3bb3f08a2468eb65808343c3289738ebd
Abdul Basit <abdulbasitswl2002@gmail.com>
2026-06-20 22:52:25 +0500

    feat: two sum using while loop

--- TwoSum.js
+++ TwoSum.js
@@ -8,10 +8,29 @@
 //             console.log("Two sum is "+array[i]+" and "+array[j]);
 //             return
 //         }
-    
+
 //     }
 // }
 
+// using while loop time complexity is $O(N \log N)$
+let array = [2, 3, 5, 6, 1, 53, 21];
+let target = 11;
+const twosum=(array,target)=>{
 
 
- 
+array.sort((a, b) => a - b);
+let left = 0;
+let right = array.length - 1;
+while (left < right) {
+  let sum = array[left] + array[right];
+  if (sum === target) {
+    console.log("Two sum is " + array[left] + " and " + array[right]);
+    return true;
+  } else if (sum < target) {
+    left++;
+  } else {
+    right--;
+  }
+  }
+}
+twosum(array,target)
