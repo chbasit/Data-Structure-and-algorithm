2c7800420b3602cf086bd52636598968157eefd7
Abdul Basit <abdulbasitswl2002@gmail.com>
2026-06-21 23:18:31 +0500

    feat: two sum using map

--- TwoSum.js
+++ TwoSum.js
@@ -13,24 +13,51 @@
 // }
 
 // using while loop time complexity is $O(N \log N)$
+// let array = [2, 3, 5, 6, 1, 53, 21];
+// let target = 11;
+// const twosum=(array,target)=>{
+
+
+// array.sort((a, b) => a - b);
+// let left = 0;
+// let right = array.length - 1;
+// while (left < right) {
+//   let sum = array[left] + array[right];
+//   if (sum === target) {
+//     console.log("Two sum is " + array[left] + " and " + array[right]);
+//     return true;
+//   } else if (sum < target) {
+//     left++;
+//   } else {
+//     right--;
+//   }
+//   }
+// }
+// twosum(array,target)
+
+
+
 let array = [2, 3, 5, 6, 1, 53, 21];
-let target = 11;
-const twosum=(array,target)=>{
+let target = 5;
 
+function twoSumBestSpeed(arr, targetVal) {
+    const numMap = new Map(); 
 
-array.sort((a, b) => a - b);
-let left = 0;
-let right = array.length - 1;
-while (left < right) {
-  let sum = array[left] + array[right];
-  if (sum === target) {
-    console.log("Two sum is " + array[left] + " and " + array[right]);
-    return true;
-  } else if (sum < target) {
-    left++;
-  } else {
-    right--;
-  }
-  }
+    for (let i = 0; i < arr.length; i++) {
+        const currentNum = arr[i];
+        const moreneed = targetVal - currentNum;
+
+        if (numMap.has(moreneed)) {
+            return [numMap.get(moreneed), i]; 
+        }
+
+        numMap.set(currentNum, i);
+    }
+    return []; 
 }
-twosum(array,target)
+const result = twoSumBestSpeed(array, target);
+if (result.length > 0) {
+    console.log("Two sum is " + array[result[0]] + " and " + array[result[1]]);
+} else {
+    console.log("No two sum pair found.");
+}
