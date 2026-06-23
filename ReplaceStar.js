0b593fd76d8fdd199dc469cd3309fd604378a02f
Abdul Basit <abdulbasitswl2002@gmail.com>
2026-06-23 15:18:51 +0500

    replace star from string

--- ReplaceStar.js
+++ ReplaceStar.js
@@ -8,3 +8,21 @@
 // let result=str.split("*").join("");
 // console.log(result);
 
+
+let str="*abdul**basit*software**engineer**";
+let array=str.split("");
+let stack=[];
+const removeAllStars=(()=>{
+
+for(let i=0;i<array.length;i++)
+{
+    let currentcharacter=array[i];
+    if(currentcharacter=="*"){
+        continue;
+    }
+    stack.push(currentcharacter);
+}
+return stack.join("");
+})
+let cleanString = removeAllStars(array);
+console.log("clean string",cleanString);
