eb7b39f9a9555eeef92a298000175cf2568b15ff
Abdul Basit <abdulbasitswl2002@gmail.com>
2026-06-23 14:20:23 +0500

    fix: unique number

--- FindDuplicateInArray.js
+++ FindDuplicateInArray.js
@@ -1,32 +1,57 @@
-let array=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,11,19,20,1,2,3]
-let duplicate=[];
-for(let i=0;i<array.length;i++){
-    for(let j=i+1;j<array.length;j++){
-        if(array[i]===array[j] && !duplicate.includes(array[i])){
-            duplicate.push(array[i]);
-        }
-    }
-}
-console.log("Duplicate number in array are "+duplicate);
+// let array=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,11,19,20,1,2,3]
+// let duplicate=[];
+// for(let i=0;i<array.length;i++){
+//     for(let j=i+1;j<array.length;j++){
+//         if(array[i]===array[j] && !duplicate.includes(array[i])){
+//             duplicate.push(array[i]);
+//         }
+//     }
+// }
+// console.log("Duplicate number in array are "+duplicate);
 
+
+let arraynumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 11, 19, 20, 1, 2, 3];
+
+let uniquenumber = [];
+let duplicates = [];
+let seen = new Set();
+
+arraynumber.forEach((num) => {
+    if (seen.has(num)) {
+        if (!duplicates.includes(num)) {
+            duplicates.push(num);
+        }
+    } else {
+        seen.add(num);
+        uniquenumber.push(num);
+    }
+});
+
+duplicates.forEach(dup => {
+    let index = uniquenumber.indexOf(dup);
+    if (index !== -1) uniquenumber.splice(index, 1);
+});
+
+console.log("Unique numbers (appearing exactly once):", uniquenumber);
+console.log("Duplicate numbers in array are: ", duplicates);
 
 // Remove duplicate from array using Set
-let duplicatenumber=[1,2,3,4,5,6,7,8,9,10,1,2,3]
-function removeDuplicate(arr){
-    return [...new Set(arr)]
-}
-console.log("Array after removing duplicate is "+ removeDuplicate(duplicatenumber));
+// let duplicatenumber=[1,2,3,4,5,6,7,8,9,10,1,2,3]
+// function removeDuplicate(arr){
+//     return [...new Set(arr)]
+// }
+// console.log("Array after removing duplicate is "+ removeDuplicate(duplicatenumber));
 
 
-let fruitlist=["apple","banana","orange","grape","apple","banana"];
-function Fruite(arr){
-    let uniqueFruite=[];
-    arr.forEach(element => {
-        if (!uniqueFruite.includes(element)) {
-            uniqueFruite.push(element);
-        }
-    })
-    return uniqueFruite;
-}
-console.log("Unique fruit in array are "+Fruite(fruitlist));
+// let fruitlist=["apple","banana","orange","grape","apple","banana"];
+// function Fruite(arr){
+//     let uniqueFruite=[];
+//     arr.forEach(element => {
+//         if (!uniqueFruite.includes(element)) {
+//             uniqueFruite.push(element);
+//         }
+//     })
+//     return uniqueFruite;
+// }
+// console.log("Unique fruit in array are "+Fruite(fruitlist));
 
